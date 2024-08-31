2f7ec8e0c26f76a7405f9236c699c7d27e27a822
pedzero <pedro9669silva@gmail.com>
2024-08-31 18:15:54 -0300

    feat: add Element class

---
+++ domain/Element.py
@@ -0,0 +1,9 @@
+from domain.Color import Color
+from domain.Color import Colors
+
+
+class Element:
+    def __init__(self, size: float, color: Color = None):
+        self.size = size
+        self.color = color if color else Colors.instance().get_next_color()
+        self.position: tuple[int, int]
