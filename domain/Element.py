c4a83e2815ce2bfd2a0d2aab79679f7a7a3f35a4
pedzero <pedro9669silva@gmail.com>
2024-09-08 22:38:31 -0300

    refactor: remove unused variables

--- domain/Element.py
+++ domain/Element.py
@@ -6,4 +6,3 @@ class Element:
     def __init__(self, size: float, color: Color = None):
         self.size = size
         self.color = color if color else Colors.instance().get_next_color()
-        self.position: tuple[int, int]
